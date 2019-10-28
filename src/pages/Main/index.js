e19de25942a649979275bd7f3476511b730705a3
Nilo Neregato <nilo.neregato@gmail.com>
2019-10-28 23:31:29 +0100

    first styled component

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
-function Main() {
-  return <h1>Main</h1>;
-}
+import { Title } from './styles';
 
-export default Main;
+export default function Main() {
+  return (
+    <Title error={false}>
+      Main
+      <small>menor</small>
+    </Title>
+  );
+}
