1454faab5404966d03c0095dc17e27f057bcc3ab
Nilo Neregato <nilo.neregato@gmail.com>
2019-10-28 23:16:17 +0100

    config router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from "react";
+import React from 'react';
+
+import Routes from './routes';
 
 function App() {
-  return (
-    <div className="App">
-      <p>Hello!</p>
-    </div>
-  );
+  return <Routes />;
 }
 
 export default App;
