4d73a20ec12d9fc3e6b5bca19990d89e95ab7f7e
Nilo Neregato <nilo.neregato@gmail.com>
2019-10-28 23:01:26 +0100

    config eslint and prettier

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import React from 'react';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
-      <p>
-        Hello!
-      </p>
+      <p>Hello!</p>
     </div>
   );
 }
